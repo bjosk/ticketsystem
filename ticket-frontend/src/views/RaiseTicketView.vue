c258ccfac6e717a4719abc2b4bb79baacff73701
Bjornar Skar <bjornar.skar@hotmail.com>
2025-06-01 17:25:17 +0200

    changes to raiseTicketView

--- ticket-frontend/src/views/RaiseTicketView.vue
+++ ticket-frontend/src/views/RaiseTicketView.vue
@@ -1,11 +1,23 @@
 <script setup>
 
 import AppNavBar from "@/components/AppNavBar.vue";
+import {useAuthStore} from "@/stores/auth.js";
+
+const auth = useAuthStore();
 </script>
 
 <template>
   <AppNavBar/>
   <div class="container bg-body-tertiary mt-4 pt-2 pb-2">
+    <div class="container p-2">
+      <label for="shortDescription" class="form-label fw-bold">Username</label>
+      <input type="text"
+             id="shortDescription"
+             class="form-control"
+             :disabled="auth.user?.role !== 'AGENT'"
+             :value="auth.user?.username"
+      />
+    </div>
     <div class="container p-2">
       <label for="shortDescription" class="form-label fw-bold">Short description</label>
       <input type="text"
